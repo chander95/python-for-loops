d1c3ffa6bea9e568e1e4c955586e96d8de027f76
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 17:02:14 -0400

    more efficient way to solve the problem

--- event-planner.py
+++ event-planner.py
@@ -6,14 +6,8 @@ items_needed = ["Grill", "Tickets", "Instruments", "Paint"]
 #For each booth type, print the booth type, the scheduled time, and the item needed.
 #Ensure that each booth type is matched with the correct schedule time and the item needed from the list provided
 
-for booth in booth_types:
-    if booth == "Food":
-        print(f"This is the {booth} booth. It will open be open starting at {schedule[1]}. We still need to get the {items_needed[0]}")
-    elif booth == "Games":
-        print(f"This is the {booth} booth. It will be open at {schedule[0]}. We're just waiting on the {items_needed[1]}")
-    elif booth == "Music":
-        print(f"This is the {booth} booth. It will open at {schedule[2]} once we get all the {items_needed[2]}")
-    elif booth == "Crafts":
-        print(f"This is the {booth} booth. It will open at {schedule[-1]} once we have all the {items_needed[-1]}")
-    else:
-        print("That's not one of the booths available today!")
+for i in range(len(booth_types)):
+    booth = booth_types[i]
+    time = schedule[i]
+    item = items_needed[i]
+    print(f"{booth} Booth - Schedule: {time} - Items Needed: {item}")
