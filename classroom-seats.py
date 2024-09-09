f2bc33f6c51d10d9d2472130e63b197e25380bf1
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 17:23:06 -0400

    started trying to solve the problem on my own. This is how it started

---
+++ classroom-seats.py
@@ -0,0 +1,11 @@
+#30 students - 5 rows
+#Each row can seat an equal number of students
+#Use a for loop with the range function to assign and print a seat number for each student
+#Seat numbers should start at 1 and increase sequentially
+
+total_students = 30
+rows = 5
+students_per_row = total_students / rows
+
+for i in range(len(students_per_row)):
+    pass
